775cbd7132f00dc24eb2b82da46247640b8938ce
improvizatoric <improvizatoric@gmail.com>
2017-06-10 20:57:56 +0300

    Second commit

--- site2/polls/admin.py
+++ site2/polls/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Post, Page
 
-admin.site.register(Post)
-admin.site.register(Page)
+# admin.site.register(Post)
+# admin.site.register(Page)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title', 'author']
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+	list_display = ['ptitle']
